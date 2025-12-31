04903fce7764c27e6ce684991efbdde2d86ffca9
rody8708 <rodycuetara@gmail.com>
2025-12-30 20:52:17 -0500

    Remove sensitive data from mail configuration
    Updated mail configuration to remove sensitive information.

--- mvc/config/mail.php
+++ mvc/config/mail.php
@@ -2,10 +2,11 @@
 return [
     'host' => 'smtp.gmail.com',
     'port' => 587,
-    'username' => 'zendrhax@gmail.com',
-    'password' => 'chdi uzbi wrip nsyu',
-    'from_email' => 'noreply@zendrhax.com',
-    'from_name' => 'zendrhax',
+    'username' => '',
+    'password' => '',
+    'from_email' => '',
+    'from_name' => '',
     'encryption' => 'tls'
 ];
+
 ?>
